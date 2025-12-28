bc2abf8cdecbd67f7a9479c77b7987c7b5d60c39
Subhajit Baidya <subhajitbaidy43@gmail.com>
2025-12-29 04:57:10 +0530

    minor changes

--- services/agent.py
+++ services/agent.py
@@ -3,6 +3,7 @@ from langsmith import traceable
 from langchain.agents import create_agent
 from dotenv import load_dotenv
 from tools.tools import retrieve_context
+from models.inputschema import UserInput
 
 
 load_dotenv(dotenv_path=".env", override=True)
@@ -16,19 +17,19 @@ class CustomerSupportAgent:
             streaming=True
         )
         self.prompt = (
-            "You are a customer support agent for a laptop manufacturing company and you specialize in providing troubleshooting steps."
+            "You are a customer support agent for a laptop company and you specialize in providing troubleshooting steps."
             "Use the tool to help answer user queries."
         )
 
     @traceable(run_type='chain')
-    def run_agent(self, query: str):
+    def run_agent(self, query: UserInput):
         tools = [retrieve_context]
         agent = create_agent(self.model, tools, system_prompt=self.prompt)
 
         response = None
 
         for event in agent.stream(
-            {"messages": [{"role": "user", "content": query}]},
+            {"messages": [{"role": "user", "content": query.message}]},
             stream_mode="values",
         ):
             response = event["messages"][-1]
@@ -43,4 +44,4 @@ if __name__ == "__main__":
         "How to fix no power issue?"
         "Once you get the answer, look up common extensions of that method."
     )
-    agent.run_agent(query)
+    agent.run_agent(UserInput(message=query))
